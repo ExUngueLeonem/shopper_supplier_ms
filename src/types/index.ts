4b94cc4259faec3a9912bae92308479c766b29c2
Leo <ujfox13@gmail.com>
2023-01-19 00:05:00 +0300

    types update

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,28 @@ export type UserInfoType = {
     email: string,
     phone: string
 }
+
+export type SupplierType = {
+    name: string;
+    address: string;
+    phone: string;
+    removed: boolean;
+    inn: string;
+    email: string;
+    description: string;
+    id: string;
+}
+
+export type CatalogItemType = {
+    parent: string | null;
+    name: string;
+    description: string | null;
+    type: string;
+    measure: string;
+    images: string | null;
+    article: number;
+    removed: boolean;
+    price: number;
+    supplierId: string;
+    id: string;
+}
