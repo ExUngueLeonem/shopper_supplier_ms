0e513739f44147e3adf173bd3b1143f23e8fc818
Leo <ujfox13@gmail.com>
2023-01-19 00:05:19 +0300

    user component

--- src/Layout/User/index.tsx
+++ src/Layout/User/index.tsx
@@ -1,26 +1,15 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
-// import { useOidc, useOidcUser } from "@axa-fr/react-oidc";
-// import useController from "Hooks/useController";
-import { useNavigate } from "react-router-dom";
 import { authStore } from "../../store/AuthStore";
+import { userStore } from "../../store/UserStore";
 import "./User.scss";
 
-// import authStore from '../../store/AuthStore';
-
 function User() {
-  const nav = useNavigate();
-  // const { logout } = useOidc();
-  // const { oidcUser, oidcUserLoadingState } = useOidcUser();
-  // const { confirm } = useController();
-
-  // console.log(oidcUser);
 
   return (
     <div className="user-panel">
       <div className="user-info">
         <div className="user-info-name">
-          {authStore.userInfo.name}
+          {userStore.userInfo.name}
         </div>
         <div className="user-button">
           <button
