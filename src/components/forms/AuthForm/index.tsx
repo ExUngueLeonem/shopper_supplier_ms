db72190241dd4d60212cdea6c798e99f5e05f020
Leo <ujfox13@gmail.com>
2023-01-19 22:55:22 +0300

    minor auth Form rework

--- src/components/forms/AuthForm/index.tsx
+++ src/components/forms/AuthForm/index.tsx
@@ -7,58 +7,65 @@ import * as Yup from 'yup';
 import styles from '../forms.module.scss';
 import { authStore } from '../../../store/AuthStore';
 import { useNavigate } from 'react-router-dom';
+import FormWrapper from '../formComponents/FormWrapper';
+import classNames from 'classnames';
+
+interface Values {
+  login: string;
+  password: string;
+  isPersistent: boolean;
+}
 
 export default function AuthForm() {
 
   const navigate = useNavigate();
 
   return (
-    <div className={styles.form_wrapper}>
-      <div className={styles.form_container}>
+    <FormWrapper>
 
-        <Formik
-          initialValues={{
-            login: '+79885515860',
-            password: 'Flagman_123',
-            isPersistent: true,
-          }}
+      <Formik
+        initialValues={{
+          login: '+79885515860',
+          password: 'Flagman_123',
+          isPersistent: true,
+        }}
 
-          validationSchema={Yup.object().shape({
-            login: Yup.string().trim().required("Обязательно к заполнению"),
-            password: Yup.string().trim().required("Обязательно к заполнению"),
-          })}
+        validationSchema={Yup.object().shape({
+          login: Yup.string().trim().required("Обязательно к заполнению"),
+          password: Yup.string().trim().required("Обязательно к заполнению"),
+        })}
 
-          onSubmit={
-            (values) => {
-              authStore.login(values).then((res) => { if (res?.status === 200) navigate("/user") })
-            }
+        onSubmit={
+          (values: Values) => {
+            authStore.login(values).then((res) => { if (res?.status === 200) navigate("/user") })
           }
-        >
-          {
-            (props) => {
-              const {
-                values
-              } = props
+        }
+      >
+        {
+          (props) => {
+            const {
+              values
+            } = props
 
-              console.log("form data values", values)
-              return (
-                <>
-                  <FormikForm>
-                    <CustomTextField formikProps={{ ...props }} name={'login'} label={'Логин'} />
-                    <CustomTextField formikProps={{ ...props }} name={'password'} label={'Пароль'} type={"password"} />
+            console.log("form data values", values)
+            return (
+              <>
+                <FormikForm>
+                  <CustomTextField formikProps={{ ...props }} name={'login'} label={'Логин'} />
+                  <CustomTextField formikProps={{ ...props }} name={'password'} label={'Пароль'} type={"password"} />
 
-                    <CustomTextField formikProps={{ ...props }} name={'isPersistent'} label={'Запомнить меня'} type={"checkbox"} />
+                  <CustomTextField formikProps={{ ...props }} name={'isPersistent'} label={'Запомнить меня'} type={"checkbox"} />
 
-                    <button type="submit" className={styles.submit_btn}>
-                      Войти
-                    </button>
-                  </FormikForm>
-                </>
-              )
-            }
+                  <button type="submit" className={ classNames(styles.submit_btn, styles.form_btn)}>
+                    Войти
+                  </button>
+                </FormikForm>
+              </>
+            )
           }
-        </Formik>
-      </div>
-    </div>
+        }
+      </Formik>
+    </FormWrapper>
+
   )
 }
