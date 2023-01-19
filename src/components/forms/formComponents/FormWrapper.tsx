20f7ad8ce54db19518ce3fadf1df555ad8265a36
Leo <ujfox13@gmail.com>
2023-01-19 23:41:15 +0300

    FormWrapper cancel_btn

--- src/components/forms/formComponents/FormWrapper.tsx
+++ src/components/forms/formComponents/FormWrapper.tsx
@@ -1,12 +1,19 @@
 import React, { ReactNode } from 'react'
+import classNames from 'classnames';
 
 import styles from './FormWrapper.module.scss';
+
+import formStyles from '../forms.module.scss';
+import { popupStore } from '../../../store/PopupStore';
 
 export default function FormWrapper({ children }: { children: ReactNode }) {
     return (
         <div className={styles.form_wrapper}>
             <div className={styles.form_container}>
                 {children}
+                <button className={classNames(formStyles.form_btn)} onClick={() => popupStore.closeForm()}>
+                    Отмена
+                </button>
             </div>
         </div>
 
