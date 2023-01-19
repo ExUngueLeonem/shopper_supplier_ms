a3bfba9e2af0c23aeef6d0c2c598ca209f5dac2a
Ilya Krukov <ujfox13@gmail.com>
2023-01-19 12:50:36 +0300

    types upd

--- src/types/index.ts
+++ src/types/index.ts
@@ -35,3 +35,54 @@ export type CatalogItemType = {
     supplierId: string;
     id: string;
 }
+
+export type OrderType = {
+    id: string;
+    number: number;
+    correlationOrderId: string;
+    created: string;
+    delivered: string | null;
+    status: string;
+    customer: {
+        id: string;
+        name: string;
+        inn: string;
+        email: string;
+        phone: string;
+        address: {
+            city: string;
+            street: string;
+            house: string;
+            index: number | null;
+            building: string | null;
+            flat: string | null;
+            entrance: string | null;
+            floor: string | null;
+            doorPhone: string | null;
+        }
+    },
+    supplier: {
+        id: string;
+        name: string;
+    }
+}
+
+export type CartType = {
+    id: string;
+    suppliers: CartSupplierType[]
+}
+
+export type CartSupplierType = {
+    id: string;
+    name: string;
+    items: CartItemType[]
+}
+
+export type CartItemType = {
+    id: string;
+    name: string;
+    price: number,
+    cost: number,
+    image: string | null;
+    amount: number,
+}
